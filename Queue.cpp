71502807796b4845f2c346426c77400e7281daf9
ndercity <117143155+ndercity@users.noreply.github.com>
2023-09-14 19:52:51 +0800

    namespace added

--- Queue.cpp
+++ Queue.cpp
@@ -2,10 +2,12 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
